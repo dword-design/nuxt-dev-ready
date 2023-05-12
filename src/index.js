533ecf21f4dbc79ac49e211b1544f2e8451a4027
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-13 00:42:39 +0200

    fix: simplified (#2)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import { delay } from '@dword-design/functions'
 import { loadNuxtConfig } from '@nuxt/kit'
 import axios from 'axios'
+import pWaitFor from 'p-wait-for'
 import portReady from 'port-ready'
 
 export default async () => {
   const config = await loadNuxtConfig()
   // See https://github.com/nuxt/nuxt/blob/main/packages/test-utils/src/server.ts#L30
   await portReady(config.devServer.port)
-  for (let i = 0; i < 50; i += 1) {
-    try {
-      await delay(100)
-
-      const result = await axios.get(config.devServer.url)
-      if (
-        !result.data.includes('__NUXT_LOADING__') &&
-        !result.data.includes('id="nuxt_loading_screen"')
-      ) {
-        return
+  await pWaitFor(
+    async () => {
+      try {
+        const result = await axios.get(
+          `http://localhost:${config.devServer.port}`,
+        )
+        if (
+          !result.data.includes('__NUXT_LOADING__') &&
+          !result.data.includes('id="nuxt_loading_screen"')
+        ) {
+          return true
+        }
+      } catch (error) {
+        return error.response.status !== 503
       }
-    } catch {
-      // continue
-    }
-  }
+
+      return false
+    },
+    { interval: 100 },
+  )
 }
