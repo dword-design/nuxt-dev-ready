2e011c637ed601c0d91bd263152e300e22634dfa
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-19 13:56:01 +0000

    fix: nuxt2

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,9 @@
 import { endent, property } from '@dword-design/functions'
 import axios from 'axios'
-import { execaCommand } from 'execa'
+import { execa, execaCommand } from 'execa'
 import fs from 'fs-extra'
 import { JSDOM } from 'jsdom'
+import ora from 'ora'
 import P from 'path'
 import kill from 'tree-kill-promise'
 import withLocalTmpDir from 'with-local-tmp-dir'
@@ -12,6 +13,17 @@ import self from './index.js'
 export default {
   async afterEach() {
     await this.resetWithLocalTmpDir()
+  },
+  before: async () => {
+    const spinner = ora('Installing Nuxt 2').start()
+    await fs.outputFile(
+      P.join('node_modules', '.cache', 'nuxt2', 'package.json'),
+      JSON.stringify({}),
+    )
+    await execaCommand('yarn add nuxt@^2', {
+      cwd: P.join('node_modules', '.cache', 'nuxt2'),
+    })
+    spinner.stop()
   },
   async beforeEach() {
     this.resetWithLocalTmpDir = await withLocalTmpDir()
@@ -27,6 +39,32 @@ export default {
     )
 
     const nuxt = execaCommand('nuxt dev')
+    try {
+      await self()
+
+      const dom = new JSDOM(
+        (await axios.get('http://localhost:3000')) |> await |> property('data'),
+      )
+      expect(dom.window.document.querySelectorAll('.foo').length).toEqual(1)
+    } finally {
+      await kill(nuxt.pid)
+    }
+  },
+  nuxt2: async () => {
+    await fs.outputFile(
+      P.join('pages', 'index.vue'),
+      endent`
+        <template>
+          <div class="foo" />
+        </template>
+      `,
+    )
+    await fs.symlink(
+      P.join('..', 'node_modules', '.cache', 'nuxt2', 'node_modules'),
+      'node_modules',
+    )
+
+    const nuxt = execa(P.join('node_modules', '.bin', 'nuxt'), ['dev'])
     try {
       await self()
 
