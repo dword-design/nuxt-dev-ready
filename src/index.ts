3ebc27cf3a358670821fb10ee4b1fdfa0e2c8d63
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-21 03:59:14 +0200

    fix: foo

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import axios from 'axios';
 import pWaitFor from 'p-wait-for';
 import portReady from 'port-ready';
 
-export default async (port = 3000) => {
+export default async (port = 3000, { interval = 500 } = {}) => {
   await portReady(port);
 
   await pWaitFor(
     async () => {
       try {
-        const { data } = await axios.get(`http://localhost:${port}`);
+        const { data } = await axios.get(`http://localhost:${port}`, { timeout: interval });
         return !data.includes('id="nuxt_loading_screen"');
       } catch (error) {
-        return error.response !== undefined && error.response.status !== 503;
+        return error.code !== 'ECONNABORTED' && error.response !== undefined && error.response.status !== 503;
       }
     },
-    { interval: 100 },
+    { interval },
   );
 };
